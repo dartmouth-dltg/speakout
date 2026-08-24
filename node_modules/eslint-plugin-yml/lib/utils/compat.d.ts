b0bca7d25284bdfddf244673a2a0c46550b801d7
Joshua Shaw <joshua.d.shaw@dartmouth.edu>
2026-08-24 12:07:31 -0400

    remove errant node_modules

--- node_modules/eslint-plugin-yml/lib/utils/compat.d.ts
+++
@@ -1,5 +0,0 @@
-import type { RuleContext, SourceCode } from "../types";
-import type { Rule, SourceCode as ESLintSourceCode } from "eslint";
-export declare function getSourceCode(context: RuleContext): SourceCode;
-export declare function getSourceCode(context: Rule.RuleContext): ESLintSourceCode;
-export declare function getFilename(context: RuleContext | Rule.RuleContext): string;
